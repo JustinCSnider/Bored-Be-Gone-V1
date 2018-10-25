04e13e7cd8cd3ad6bd50c0589e3796e96fb97982
Justin Snider <jcsnider100@gmail.com>
2018-10-25 10:26:49 -0600

    Created Basic storyboard structure and started on login ui and functionality

---
+++ BoredBeGone/Controllers/IntroToLoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  IntroToLoginViewController.swift
+//  BoredBeGone
+//
+//  Created by Justin Snider on 10/25/18.
+//  Copyright © 2018 Justin Snider. All rights reserved.
+//
+
+import UIKit
+
+class IntroToLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToIntroToLogin(segue: UIStoryboardSegue) {
+        
+    }
+
+}
---
+++ BoredBeGone/Controllers/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BoredBeGone
+//
+//  Created by Justin Snider on 10/25/18.
+//  Copyright © 2018 Justin Snider. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if let destinationView = segue.destination as? forgotUsernameOrPasswordViewController {
+            if segue.identifier == "forgotUsername" {
+                destinationView.navigationItem.title = "Forgot Username"
+            } else if segue.identifier == "forgotPassword" {
+                destinationView.navigationItem.title = "Forgot Password"
+            }
+        }
+    }
+
+
+}
+
