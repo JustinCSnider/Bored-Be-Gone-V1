a6cdcdfa8f34d4283938267b539e80ae95dd6ccd
Justin Snider <jcsnider100@gmail.com>
2018-11-08 10:24:28 -0700

    Added basic functionality to the solve my boredom button

--- BoredBeGone/Controllers/HomeViewController.swift
+++ BoredBeGone/Controllers/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var subView: UIView!
+    @IBOutlet weak var solutionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +18,14 @@ class HomeViewController: UIViewController {
         subView.layer.borderColor = UIColor.lightGray.cgColor
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func solveBoredomTapped(_ sender: UIButton) {
+        let randomFirstIndex = Int.random(in: 0 ..< firstPartAnswerList.count)
+        let randomSecondIndex = Int.random(in: 0 ..< secondPartAnswerList.count)
+        
+        solutionLabel.text = firstPartAnswerList[randomFirstIndex] + secondPartAnswerList[randomSecondIndex]
+    }
+    
     
     @IBAction func unwindToHomePage(segue: UIStoryboardSegue) {
         
---
+++ BoredBeGone/Models/SolutionData.swift
@@ -0,0 +1,17 @@
+//
+//  SolutionData.swift
+//  BoredBeGone
+//
+//  Created by Justin Snider on 11/8/18.
+//  Copyright © 2018 Justin Snider. All rights reserved.
+//
+
+import Foundation
+
+var firstPartAnswerList: [String] = ["Run",
+                                  "Eat",
+                                  "Drive"]
+
+var secondPartAnswerList: [String] = [" with some friends",
+                                      " by your self",
+                                      " somewhere new"]
